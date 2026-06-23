d095673b5507447d40b758b96d299a54d0b14794
gurunathan-prodapt <gurunathan.t@prodapt.com>
2026-06-23 16:15:38 +0530

    feat(build): DW.BERT_AUSD_BP_TA_RN_DA_VDA_TK

---
+++ bq_ddl/your_metadata_dataset.dwtk_meldungen.sql
@@ -0,0 +1,7 @@
+-- Legacy Source: N/A (Derived from Oracle SQL script d_ausd_bp_ta_rn_da_vda_tk.sql)
+-- Job: DW.BERT_AUSD_BP_TA_RN_DA_VDA_TK
+CREATE TABLE IF NOT EXISTS `your_metadata_dataset.dwtk_meldungen`
+(
+  job_kennung STRING,
+  timecreated TIMESTAMP
+);
